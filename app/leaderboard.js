bad3640c18f48b7934d3ef7c7d1926717bb99f48
Wyatt Bartlett <wyattmmbartlett@gmail.com>
2016-10-11 13:31:00 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,1 +1,30 @@
+PlayersList = new Mongo.Collection('players');
 
+if (Meteor.isClient) {
+
+  Template.leaderboard.helpers({
+    'player': function () {
+      return PlayersList.find();
+    },
+    'selectedClass': function () {
+      const playerId = this._id;
+      const selectedPlayer = Session.get('selectedPlayer');
+      if (playerId === selectedPlayer) {
+        return 'selected';
+      }
+    },
+  });
+
+  Template.leaderboard.events({
+    'click .player': function () {
+      let playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    },
+  });
+
+  console.log("Hello client");
+}
+
+if (Meteor.isServer) {
+  console.log("Hello server");
+}
